f303c3856316a3c4c5f981382889f8b4f24e4049
owo <owo@Jennifers-MacBook.local>
2021-08-21 16:00:31 +1000

    Stephen- checkboxes

--- SustainableMe/ViewController.swift
+++ SustainableMe/ViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var tree: UIImageView!
-    @IBOutlet weak var coins: UITextView!
+    @IBOutlet weak var coinLabel: UITextView!
     @IBOutlet weak var snooze: UIImageView!
+    @IBOutlet weak var checkBox: UIButton!
+    
+    var coinCount = 0
+    
+    @IBAction func check(_ sender: UIButton) {
+        if ((sender.currentBackgroundImage) == nil){
+            coinCount += 1
+            sender.setBackgroundImage(UIImage(named:"checked.png"), for: .normal)
+        }
+        else {
+            coinCount -= 1
+            sender.setBackgroundImage(nil, for: .normal)
+        }
+        coinLabel.text = " \(coinCount)/100"
+    
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
